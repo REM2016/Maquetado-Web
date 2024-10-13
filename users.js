7600d7c9e5fdf64eb6b938f48857f3faeee95d27
REM2016 <celsoperez878@gmail.com>
2024-10-12 19:26:42 -0600

    nuevos comentarios

--- users.js
+++ users.js
@@ -1,11 +1,13 @@
 const bcrypt = require('bcryptjs');
 
 const users = [
+  //usuario 1 se almacena el usuario y la contraseña 
   {
     id: 1,
     username: 'usuario1',
     password: bcrypt.hashSync('password123', 10) // Contraseña encriptada
   },
+  //usuario 2 se almacena el usuario y la contraseña 
   {
     id: 2,
     username: 'usuario2',
